a010575b2c2b3f9b749c46db9fdaa30293418180
seedubjay <cwj@dhcp-10-249-79-60.eduroam.wireless.private.cam.ac.uk>
2020-04-08 12:47:24 +0100

    all the words

--- _posts/2020-03-31-terrain-generation.md
+++ _posts/2020-03-31-terrain-generation.md
@@ -1,16 +1,57 @@
 ---
 layout: post
-title: Terrain Generation
-subtitle: Making machines less mechanical
+title: Making Machines Less Mechanical
+subtitle: hand-drawn lines & terrain generation
 scripts: ['core', 'perlin', 'terrain-generation']
 ---
-## Perlin noise
+
+Computers aren't very good at _blending in_. It usually isn't because they aren't quick enough to keep up with humans (despite how good you think you are at [spotting traffic lights...](https://www.ceros.com/originals/recaptcha-waymo-future-of-self-driving-cars/)). Often, its simply because computers are too efficient or too precise to seem human. 
+
+Ironically, its quite tricky for computers to appear more natural than they actually are. This issue crops up all the time, from creating creatures in video games which move organically, to creating a voice assistant (Siri, Alexa, etc...) which speaks with a natural-sounding cadence, to generating realistic clouds in the sky and rocks on the ground in an animated movie.
+
+Alas, all of these problems are quite hard (and the focus of ongoing research) so we'll stick to something simpler. 
+
+## Noise
+
+A stream of unpredictable data is called _noise_. Static from a radio or a TV is noise in the truest sense of the word, because every single waveform and pixel is completely random. However, you could equally call the path of a drunk person trying to walk home 'noise' - they'll walk vaguely in the right direction, but you've got no chance of guessing where any particular step will take them...
+
+This second type of noise is much more interesting. By restricting the randomness, we can create interesting curves and shapes that are _unpredictable_ but _controllable_.
+
+In 1983, Ken Perlin created a new type of noise called 'Perlin noise'. Just like a drunk person stumbling home, Perlin noise is random at every point, but there is an overall shape and cohesiveness to the random values. Perlin noise works best in 2D, so you can easily create pictures like this:
+
 <figure>
-{% include canvas.html id="canvas2" reload="dark" height="500" %}
+{% include canvas.html id="canvas0" reload="light" %}
+<figcaption>
+<p class="caption">2D Perlin noise</p>
+</figcaption>
+</figure>
+
+There are a few obvious properties to Perlin noise. Firstly, the areas with 'extreme' values (i.e. very light or very dark patches) never last for very long, and the grey sections in between them fill the gaps. Secondly, these changes in colour always happen gradually, shifting from light to dark bit by bit.
+
+Of course, there are also faults! If you reload the picture a few times, you might start to notice some odd blockiness and straight lines... But, as we'll see, these faults can be covered up.
+
+The success of Perlin noise didn't just come from the noise it produced, but also from how fast it produced it. These little faults were a small price to pay for a fast algorithm, especially back in 1983 when computing time was slow and expensive.
+
+## Octaves
+
+Perlin noise is so versatile because it can be used in many contexts. Above, we converted its values into colours, but we can also think of them as heights, with light patches being the 'hills' and dark patches being the 'valleys'. 
+
+Our eventual goal is to create noise which seems messy enough to seem natural. Perlin noise gets us close, but it ebbs and flows so consistently from value to value that it seems artificial. 
+
+Why is this? The real world isn't consistent! It is sometimes smooth, sometimes jagged, and sometimes completely flat. To recreate this, we will add _octaves_ to the noise. 
+
+The 0<sup>th</sup> octave is the original noise, like what we saw above. The 1<sup>st</sup> octave is identical, but shrunk to be _half the height_ (i.e. amplitude) and changing _twice as fast_ (i.e. frequency). Each sucessive octave does the same, so the 4<sup>th</sup> octave is only 1/16<sup>th</sup> the height and changing 16 times faster than the original noise.
+
+These octaves are all 'added together' to create noise that can simultaneously change huge amounts very slowly, but also change in tiny fractions frequently.
+
+You can see what different combinations look like here:
+
+<figure>
+{% include canvas.html id="canvas2" reload="dark" %}
 <div class="overlay overlay-top overlay-right">
     <div class="btn-group">
     {% for i in (0..4) %}
-    <button type="button" class="btn btn-secondary btn-sm no-focus active" data-toggle="button" aria-pressed="true" onclick="toggle_button_canvas2({{ i }})">{{ i }}</button>
+    <button type="button" class="btn btn-secondary btn-sm no-focus" data-toggle="button" aria-pressed="{% if i == 0 %}true{% else %}false{% endif %}" onclick="toggle_button_canvas2({{ i }})">{{ i }}</button>
     {% endfor %}
     </div>
 </div>
@@ -19,59 +60,75 @@ scripts: ['core', 'perlin', 'terrain-generation']
 </figcaption>
 </figure>
 
-
-<!-- ## 2D stacked noise
-<figure>
-{% include canvas.html id="canvas0" reload="light" interactive="" %}
-</figure> -->
+It works in 2D as well!
 
 <figure>
-<div class="canvas-group canvas-group-2">
-    {% include canvas.html id="canvas3a" width="300" height="300" reload="light" %}
-    {% include canvas.html id="canvas3b" width="400" height="400" %}
+{% include canvas.html id="canvas7" width="400" reload="light" %}
+<div class="overlay overlay-top overlay-right">
+    <div class="btn-group">
+    {% for i in (0..4) %}
+    <button type="button" class="btn btn-light btn-sm no-focus" data-toggle="button" aria-pressed="{% if i == 0 %}true{% else %}false{% endif %}" onclick="toggle_button_canvas7({{ i }})">{{ i }}</button>
+    {% endfor %}
+    </div>
 </div>
 <figcaption>
-    <input type="range" min="0" max="1" value=".4" step="0.01" class="slider" id="canvas2-slider">
+<p class="caption">Add or remove octaves from the noise</p>
 </figcaption>
 </figure>
+
+Perlin noise is a powerful tool once we start modifying octaves and many of its other parameters. With very minimal effort, we can start creating new textures and designs.
+
+## Colour palettes
+
+We may have a map of noise, but how are we actually visualising it? Behind the scenes, the Perlin noise algorithm produces a value between 0 and 1 for every pixel in the image. It's then up to us to define how that value should be turned into a colour, like a 'colour palette' of sorts. For instance, up until now we've been turning 0's into black, 1's into white, and blending everything in between. But now, we're going to try convert our noise into a set of islands. Here's our new colour palette:
+
+<figure>
+    {% include canvas.html id="canvas4a" class="no-outline" width="800" height="30" %}
+    <figcaption><p class="caption">Greyscale colour palette</p></figcaption>
+    {% include canvas.html id="canvas4c" class="no-outline" width="800" height="30" %}
+    <figcaption><p class="caption">Islands colour palette</p></figcaption>
+</figure>
+
+And so with barely any effort, we can create this:
 
 <figure>
 {% include canvas.html id="canvas1" reload="light" interactive="" %}
 <figcaption><p class="caption">Hover to reveal the underlying height map</p></figcaption>
 </figure>
-test
-test
-test
-test
-test
-test
-test
 
-test
-testtest
-test
-testtest
+Want to create an island surrounded completely by water? We can adjust the colour palette for pixels further away from the centre of the map so that more and more noise is converted into water closer to the edges.
 
-test
-testtest
-
-test
-testtest
-test
-test
-
-test
-test
-test
-test
-test
-test
-
-test
-test
-test
+<figure>
+    {% include canvas.html id="canvas3a" width="400" reload="light" %}
+    {% include canvas.html id="canvas3b" class="no-outline" width="800" height="150" %}
+<figcaption>
+    <input type="range" min="0" max="1" value=".3" step="0.01" class="slider" id="canvas2-slider">
+    <p class="caption">Adjust the colour palettes further from the center</p>
+</figcaption>
+</figure>
 
 
-test
-test
-test
+## Gallery
+
+Terrain generation is just the beginning of what can be done with noise maps. Here's a few ideas you can play with yourself...
+
+### Hand-drawn sketches
+
+<figure>
+{% include canvas.html id="canvas6" reload="dark" %}
+<figcaption>
+    <input type="range" min="0" max="1" value=".4" step="0.01" class="slider" id="canvas6-slider">
+    <p class="caption">Adjust the messiness of the sketch</p>
+</figcaption>
+</figure>
+
+### Clouds
+
+<figure>
+{% include canvas.html id="canvas8" width="400" reload="dark" %}
+</figure>
+
+### Blood vessels
+<figure>
+{% include canvas.html id="canvas9" width="400" reload="dark" %}
+</figure>
--- assets/js/terrain-generation.js
+++ assets/js/terrain-generation.js
@@ -1,4 +1,4 @@
-function perlin(x,y,{scale = 1, octaves = 1, lacunarity = 2.0, persistence = 0.5, stretch = 1.0}) {
+function perlin(x,y,{scale = 1, octaves = 1, lacunarity = 2.0, persistence = 0.5, stretch = 1}) {
     let total = 0,
         frequency = 1/scale,
         amplitude = 1,
@@ -18,10 +18,7 @@ let clamp =(v, minv, maxv) => Math.min(Math.max(v,minv),maxv);
 let sinoid = (x,k) => (x-x*k)/(k-Math.abs(x)*2*k+1);
 
 // [-1,1] -> [0,256)
-function applyGreyscale(value) {
-    let v = ((value+1)/2)*256;
-    return [v,v,v,255];
-}
+let colourGreyscale = v => [v*256,v*256,v*256,255];
 
 function interpolate(y1, y2, x1=0,x2=1) {
     return v => {
@@ -30,26 +27,31 @@ function interpolate(y1, y2, x1=0,x2=1) {
     }
 }
 function colourInterpolate(c1, c2, x1=0, x2=1) {
-    return v => c1.map((c,i) => interpolate(c,c2[i],x1,x2)(v));
+    return v => c1.map((c,i) => Math.floor(interpolate(c,c2[i],x1,x2)(v)));
 }
 
-function colourChunks(groups) {
+function colourChunks(colours, points = null) {
+    if (points === null) {
+        points = [];
+        for (let i in colours) points.push(i/colours.length);
+        points.push(1);
+    }
+    console.assert(points.length == colours.length + 1)
     return (value) => {
-        for (var c of groups) {
-            if (value <= c[0]) {
-                if (typeof c[1] == 'function') return c[1](value);
-                else return c[1];
+        for (let i = 0; i < colours.length; i++) {
+            if (value <= points[i+1]) {
+                if (colours[i] === null) return colourInterpolate(colours[i-1],colours[i+1],points[i],points[i+1])(value);
+                else return colours[i];
             }
         }
-        return [0,0,0,255];
+        return colours[colours.length-1];
     }
 }
 
 // f(value,distance) adjusts value according to distance from the center (ranges from 0 to 1)
 function heightMapByCenter(f) {
-    return (value, {x, y, width, height}) => {
-        let d = Math.hypot(x-width/2, y-height/2);
-        return f(value, clamp(d/width*2,0,1));
+    return (value, {x, y, width, height, r = Math.hypot(x-width/2, y-height/2)/width*2}) => {
+        return f(value, clamp(r,0,1));
     }
 }
 
@@ -103,17 +105,14 @@ function graphGenerator(canvasID, defaultOptions) {
 }
 
 function terrainGenerator(canvasID, defaultOptions = {}) {
-    let canvas = document.getElementById(canvasID);
+    let {canvas, draw: drawImage} = imageGenerator(canvasID);
     if (!canvas) return {};
-    let ctx = canvas.getContext("2d");
-    let image = ctx.createImageData(canvas.width, canvas.height);
-    let data = image.data;
 
     let heights = new Array(canvas.width);
     for (let x = 0; x < canvas.width; x++) heights[x] = new Array(canvas.height);
 
     function generate(options = {}) {
-        let {perlinOptions, heightMapper = v=>v} = {
+        let {perlinOptions, heightMapper = v=>v, autoDraw = false} = {
             ...defaultOptions,
             ...options
         };
@@ -121,88 +120,112 @@ function terrainGenerator(canvasID, defaultOptions = {}) {
         
         for (let x = 0; x < canvas.width; x++) {
             for (let y = 0; y < canvas.height; y++) {
-                heights[x][y] = heightMapper(perlin(x,y,perlinOptions), {x: x, y: y, width: canvas.width, height: canvas.height})
+                heights[x][y] = heightMapper(perlin(x,y,perlinOptions), {x: x, y: y, width: canvas.width, height: canvas.height});
             }
         }
+
+        if (autoDraw) draw();
     }
 
     function draw(options = {}) {
         let {
+            colourMapper = v => colourGreyscale((v+1)/2),
+        } = {...defaultOptions, ...options};
+        drawImage({
+            getPixel: (x,y) => colourMapper(heights[x][y], {x: x, y: y, width: canvas.width, height: canvas.height}),
+            ...defaultOptions,
+            ...options
+        });
+    }
+
+    return {canvas: canvas, generate: generate, draw: draw};
+}
+
+function imageGenerator(canvasID, defaultOptions = {}) {
+    let canvas = document.getElementById(canvasID);
+    if (!canvas) return {};
+    let ctx = canvas.getContext("2d");
+    let image = ctx.createImageData(canvas.width, canvas.height);
+    let data = image.data;
+
+    function draw(options = {}) {
+        console.time(`draw ${canvasID}`);
+        let {
+            getPixel,
             x1 = 0,
             x2 = 1e9,
             y1 = 0,
             y2 = 1e9,
-            colourMapper = applyGreyscale,
         } = {...defaultOptions, ...options};
         x1 = Math.max(Math.round(x1),0);
         y1 = Math.max(Math.round(y1),0);
         x2 = Math.min(Math.round(x2),canvas.width-1);
         y2 = Math.min(Math.round(y2),canvas.height-1);
 
-        let changedX1 = 1e9,
-            changedY1 = 1e9,
-            changedX2 = -1e9,
-            changedY2 = -1e9;
+        let bbox = {}
 
         for (let x = x1; x <= x2; x++) {
             for (let y = y1; y <= y2; y++) {
-                let [r,g,b,a] = colourMapper(heights[x][y], {x: x, y: y, width: canvas.width, height: canvas.height}),
+                let [r,g,b,a] = getPixel(x,y,{width: canvas.width, height: canvas.height});
                     i = (x+y*canvas.width)*4;
                 if (data[i] != r || data[i+1] != g || data[i+2] != b || data[i+3] != a) {
-                    changedX1 = Math.min(changedX1,x);
-                    changedY1 = Math.min(changedY1,y);
-                    changedX2 = Math.max(changedX2,x);
-                    changedY2 = Math.max(changedY2,y);
+                    bbox.x1 = Math.min(bbox.x1 || 1e9, x1);
+                    bbox.y1 = Math.min(bbox.y1 || 1e9, y1);
+                    bbox.x2 = Math.max(bbox.x2 || -1e9, x2);
+                    bbox.y2 = Math.max(bbox.y2 || -1e9, y2);
+                    data[i] = r;
+                    data[i+1] = g;
+                    data[i+2] = b;
+                    data[i+3] = a;
                 }
-                data[i] = r;
-                data[i+1] = g;
-                data[i+2] = b;
-                data[i+3] = a;
             }
         }
 
-        if (changedX1 <= changedX2 && changedY1 <= changedY2) {
-            ctx.putImageData(
-                image,
-                0,
-                0,
-                changedX1,
-                changedY1,
-                changedX2-changedX1+1,
-                changedY2-changedY1+1
-            );
-        }
+        if (typeof bbox.x1 != 'undefined') ctx.putImageData(
+            image,
+            0,
+            0,
+            bbox.x1,
+            bbox.y1,
+            bbox.x2-bbox.x1+1,
+            bbox.y2-bbox.y1+1,
+        );
+        console.timeEnd(`draw ${canvasID}`);
     }
 
-    return {canvas: canvas, generate: generate, draw: draw};
+    return {canvas: canvas, draw: draw};
 }
 
-let colourscheme = [
-    [-.3,   [0,0,100,255]],
-    [0,     colourInterpolate([0,0,100,255],[35,65,134,255],-.3,0)],
-    [0.1,   [198,166,100,255]],
-    [1,     [11,102,35,255]]
-]
+let terrainColourMap = colourChunks(
+    [[0,0,100,255], null, [35,65,134,255], [198,166,100,255], [11,102,35,255]],
+    [-1,-.3,0,0,.1,1],
+)
+
+let terrainGreyscaleMap = v => colourGreyscale((sinoid(v,-.2)+1)/2);
 
 // canvas0: noise
 let reload_canvas0;
-(() => {
+window.addEventListener('load', () => {
     let {canvas, generate, draw} = terrainGenerator("canvas0", {
-        perlinOptions: {scale:200,octaves:4,lacunarity:2.0,persistence:0.5,stretch:2.0},
+        perlinOptions: {scale:100},
+        colourMapper: terrainGreyscaleMap,
+        autoDraw: true,
     });
     if (!canvas) return;
     generate();
-    draw();
-    reload_canvas0 = () => {generate(); draw()}
-})();
+    reload_canvas0 = generate;
+});
 
 // canvas1: interactive map revealing noise upon hover
 let reload_canvas1;
-(() => {
+window.addEventListener('load', () => {
+    const rangeMap = (v, d) => interpolate(interpolate(-.8, -1)(d), interpolate(1, -.4)(d), -1, 1)(v);
     let {canvas, generate, draw} = terrainGenerator("canvas1", {
-        perlinOptions: {scale:130,octaves:4,lacunarity:2,persistence:0.5,stretch:2.0},
-        heightMapper: heightMapByCenter((v,d) => interpolate(interpolate(-.9,-1)(d), interpolate(1,-.4)(d),-1,1)(v)),
-        colourMapper: colourChunks(colourscheme),
+        perlinOptions: {scale:120,octaves:4,stretch:1.7},
+        //heightMapper: heightMapByCenter((v,d) => interpolate(interpolate(-.8,-1)(d), interpolate(1,-.4)(d),-1,1)(v)),
+        //colourMapper: (v,o) => terrainColourMap(heightMapByCenter(rangeMap)(v,o)),
+        colourMapper: terrainColourMap,
+        autoDraw: true,
     });
     if (!canvas) return;
     
@@ -214,11 +237,11 @@ let reload_canvas1;
         window.cancelAnimationFrame(animationID);
         animationID = requestAnimationFrame(() => {
             draw({
-                colourMapper: (v, {x, y}) => {
+                colourMapper: (v, {x, y, width, height}) => {
                     if (Math.hypot(mouse.x-x, mouse.y-y) < mouseRadius) {
-                        return applyGreyscale(sinoid(v,-.3));
+                        return terrainGreyscaleMap(v);
                     } else {
-                        return colourChunks(colourscheme)(v);
+                        return terrainColourMap(v);
                     }
                 },
                 x1: Math.min(prevMouse.x, mouse.x) - mouseRadius, 
@@ -243,26 +266,26 @@ let reload_canvas1;
     };
     
     generate();
-    draw();
-    reload_canvas1 = () => {generate(); draw()};
-})();
+    reload_canvas1 = generate;
+});
 
 // CANVAS2: octaves graph
 let reload_canvas2;
 let toggle_button_canvas2;
-(() => {
+window.addEventListener('load', () => {
     let octaves = 5,
         scale = .5,
         stretch = 1.5,
-        lacunarity = 2,
-        persistence = .5,
+        lacunarity = 2.2,
+        persistence = 1/lacunarity,
         maxAmplitude = (1-persistence**octaves)/(1-persistence);
 
-    let visible = Array(octaves).fill(true);
+    let visible = Array(octaves).fill(false);
+    visible[0] = true;
     let seed = Math.random();
 
     let {draw} = graphGenerator("canvas2", {
-        lines: [(x) => {
+        lines: [x => {
             let total = 0,
                 amplitude = 1,
                 frequency = 1/scale;
@@ -274,7 +297,6 @@ let toggle_button_canvas2;
             }
             return total / maxAmplitude * stretch;
         }],
-        x_axis: true,
         x_lim: [0,1],
     });
 
@@ -285,39 +307,46 @@ let toggle_button_canvas2;
         visible[n] = visible[n] ^ true;
         draw();
     }
-})();
+});
 
-// canvas3: 
+// canvas3: adjust the height map at the edges
 let reload_canvas3a;
-(() => {
-    let rangeMin = interpolate(-.9,-1),
-        rangeMax = interpolate(1,-.2),
-        rangeMap = (v,d) => interpolate(rangeMin(d),rangeMax(d),-1,1)(v);
+window.addEventListener('load', () => {
+    let slider = document.getElementById('canvas2-slider');
+
+    let rangeMin = interpolate(interpolate(-.2,-1)(slider.value),-1)
+        rangeMax = interpolate(1,interpolate(-1,1)(slider.value));
+        rangeMap = (v,d) => interpolate(rangeMin(d),rangeMax(d),-1,1)(v),
+        colourMap = (v,o) => terrainColourMap(heightMapByCenter(rangeMap)(v,o));
 
     let {canvas: canvasMap, generate, draw: drawMap} = terrainGenerator("canvas3a", {
-        perlinOptions: {scale:50,octaves:4,lacunarity:2,persistence:0.5,stretch:2.0},
-        colourMapper: (v,o) => colourChunks(colourscheme)(heightMapByCenter(rangeMap)(v,o), o),
+        perlinOptions: {scale:100,octaves:4,lacunarity:2,persistence:0.5,stretch:1.7},
+        colourMapper: colourMap,
+        autoDraw = true,
     });
     if (!canvasMap) return;
     
     generate();
-    drawMap();
-    reload_canvas3a = () => {generate(); drawMap()};
+    reload_canvas3a = generate;
 
-    let {canvas: canvasGraph, draw: drawGraph} = graphGenerator("canvas3b", {
-        lines: [
-            x => rangeMin(Math.abs(x)),
-            x => rangeMax(Math.abs(x))
-        ],
+    let strips = 15;
+    let gapPerStrip = 0.5
+
+    let {draw: drawGraph} = imageGenerator("canvas3b", {
+        getPixel: (x,y,{width,height}) => {
+            let stripWidth = width / (strips + gapPerStrip*(strips-1));
+            let d = x % (stripWidth*(1+gapPerStrip));
+            if (d > stripWidth) return [255,255,255,255];
+            x = x - d + stripWidth/2;
+            return colourMap(-y/height*2+1, {r: Math.abs(x/width*2-1)});
+        }
     });
-    if (!canvasGraph) return;
 
     drawGraph();
 
-    let slider = document.getElementById('canvas2-slider'),
-        animationID = 0;
-
+    let animationID = 0;
     slider.oninput = () => {
+        rangeMin = interpolate(interpolate(-.2,-1)(slider.value),-1)
         rangeMax = interpolate(1,interpolate(-1,1)(slider.value));
         window.cancelAnimationFrame(animationID);
         animationID = requestAnimationFrame(() => {
@@ -325,4 +354,179 @@ let reload_canvas3a;
             drawGraph();
         });
     }
-})();
+});
+
+// canvas4: colour palette
+window.addEventListener('load', () => {
+    imageGenerator("canvas4a", {
+        getPixel: (x,_,{width}) => terrainGreyscaleMap(x/width*2-1)
+    }).draw();
+
+    // imageGenerator("canvas4b", {
+    //     getPixel: (x,_,{width}) => skyColourMap(x/width*2-1)
+    // }).draw();
+
+    imageGenerator("canvas4c", {
+        getPixel: (x,_,{width}) => terrainColourMap(x/width*2-1)
+    }).draw();
+});
+
+// canvas5: terrain without falling away into the ocean
+let reload_canvas5;
+window.addEventListener('load', () => {
+    let {canvas, generate} = terrainGenerator("canvas5", {
+        perlinOptions: {scale:130,octaves:4,lacunarity:2,persistence:0.5,stretch:2.0},
+        colourMapper: terrainColourMap,
+        autoDraw: true,
+    });
+    if (!canvas) return;
+    
+    generate();
+    reload_canvas5 = generate;
+});
+
+let reload_canvas6;
+window.addEventListener('load', () => {
+    let slider = document.getElementById('canvas6-slider');
+
+    let canvas = document.getElementById("canvas6");
+    if (!canvas) return {};
+    let ctx = canvas.getContext("2d");
+
+    let f = x => canvas.height*.2 * Math.sin((x-canvas.width/2) / (canvas.width*.3) * 2 * Math.PI)
+
+    function draw() {
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        let h = .4;
+
+        // x-axis
+        ctx.lineWidth = 1;
+        h++;
+        ctx.beginPath();
+        for (let x = canvas.width*.1; x <= canvas.width*.9; x++) {
+            let y = slider.value**2 * 30 * perlin(x/20,h,{octaves: 4});
+            let lx = x;
+            let ly = canvas.height/2 - y;
+
+            if (x == 0) ctx.moveTo(lx,ly);
+            else ctx.lineTo(lx,ly);
+        }
+        ctx.stroke();
+
+        // y-axis
+        ctx.lineWidth = 1;
+        h++;
+        ctx.beginPath();
+        for (let y = canvas.height*.2; y <= canvas.height*.8; y++) {
+            let x = slider.value**2 * 30 * perlin(y/20,h,{octaves: 4});
+            let lx = canvas.width/2 - x;
+            let ly = y;
+
+            if (y == 0) ctx.moveTo(lx,ly);
+            else ctx.lineTo(lx,ly);
+        }
+        ctx.stroke();
+
+        ctx.lineWidth = 3;
+        h++;
+        ctx.beginPath();
+        for (let x = canvas.width*.2; x <= canvas.width*.8; x++) {
+            let y = f(x);
+            let dx = 0.1, dy = y - f(x-dx), s = slider.value**2 * 100 * perlin(x/50,h,{octaves: 6}) / Math.hypot(dx,dy);
+            let lx = x + dy * s; // perpendicular to direction of curve
+            let ly = y - dx * s;
+            ly = canvas.height/2 - ly;
+
+            if (x == 0) ctx.moveTo(lx,ly);
+            else ctx.lineTo(lx,ly);
+        }
+        ctx.stroke();
+    }
+
+    reload_canvas6 = () => {noise.seed(Math.random()); draw()}
+    reload_canvas6();
+
+    let animationID = 0;
+    slider.oninput = () => {
+        window.cancelAnimationFrame(animationID);
+        animationID = requestAnimationFrame(() => {
+            draw();
+        });
+    }
+});
+
+let reload_canvas7;
+let toggle_button_canvas7;
+window.addEventListener('load', () => {
+    let octaves = 5,
+        scale = 50,
+        stretch = 1.5,
+        lacunarity = 2,
+        persistence = 1/lacunarity,
+        maxAmplitude = (1-persistence**octaves)/(1-persistence);
+
+    let visible = Array(octaves).fill(false);
+    visible[0] = true;
+    let seed = Math.random();
+
+    let {draw} = imageGenerator("canvas7", {
+        getPixel: (x,y) => {
+            let total = 0,
+                amplitude = 1,
+                frequency = 1/scale;
+            noise.seed(seed);
+            for (let i = 0; i < octaves; i++) {
+                if (visible[i]) total += noise.perlin2(x * frequency, y * frequency) * amplitude;
+                frequency *= lacunarity;
+                amplitude *= persistence;
+            }
+            return colourGreyscale(clamp(total / maxAmplitude * stretch,-1,1)/2+.5);
+        },
+    });
+
+    draw();
+
+    reload_canvas7 = () => {seed = Math.random(); draw()};
+    toggle_button_canvas7 = n => {
+        visible[n] = visible[n] ^ true;
+        draw();
+    }
+});
+
+// canvas8: clouds in the sky
+let reload_canvas8;
+window.addEventListener('load', () => {
+    let colourMap = colourChunks(
+        [[173,216,230,255],null,[255,255,255,255]],
+        [-1,0,.5,1]
+    );
+
+    let {canvas, generate} = terrainGenerator("canvas8", {
+        perlinOptions: {scale:150, octaves: 6, stretch: 2},
+        colourMapper: colourMap,
+        autoDraw: true,
+    });
+    if (!canvas) return;
+
+    generate();
+    reload_canvas8 = generate;
+});
+
+// canvas8: blood vessels
+let reload_canvas9;
+window.addEventListener('load', () => {
+    let colourMap = v => colourChunks(
+        [[78, 119, 133, 255],null,[255, 123, 98, 255],null,[255,219,172,255]],
+        [0,.01,.03,.03,.05,1])
+        (Math.abs(v));
+
+    let {canvas, generate} = terrainGenerator("canvas9", {
+        perlinOptions: {scale:150, octaves: 4, stretch: 1.5},
+        colourMapper: colourMap,
+        autoDraw: true,
+    });
+    if (!canvas) return;
+
+    generate();
+    reload_canvas9 = generate;
+});
