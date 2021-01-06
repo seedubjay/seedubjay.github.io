212fda6bc7a4573cfcfe151cf8edd4b10738e96d
seedubjay <cwj@dhcp-10-249-65-243.eduroam.wireless.private.cam.ac.uk>
2021-01-06 19:17:05 +0000

    save button for svg

--- assets/js/wikipedia-clicks.js
+++ assets/js/wikipedia-clicks.js
@@ -167,20 +167,35 @@ window.addEventListener('load', () => {
         const boxWidth = +viewbox[2];
         const boxHeight = +viewbox[3];
 
-        const graphMargin = {top: 20, right: 5, bottom: 5, left: 160}
+        const graphMargin = {top: 30, right: 5, bottom: 5, left: 160}
 
         const width = boxWidth - graphMargin.left - graphMargin.right;
         const height = boxHeight - graphMargin.top - graphMargin.bottom;
 
         data = data.splice(0,30)
 
+        svg.append("rect")
+            .attr("width", "100%")
+            .attr("height", "100%")
+            .attr("fill", "white")
+
         svg.append("text")
             .attr("text-anchor", "middle")
             .attr("x", boxWidth/2)
-            .attr("y", 0)
+            .attr("y", 2)
             .attr("dy", "1em")
             .attr("font-size", 12)
-            .text("(estimated) top pages by betweenness centrality")
+            .text("Most useful pages for beating the Wikipedia Game")
+        svg.append("text")
+            .attr("text-anchor", "middle")
+            .attr("x", boxWidth/2)
+            .attr("y", 18)
+            .attr("dy", "1em")
+            .attr("font-size", 8)
+            .text("(as defined by betweenness centrality -- details in comments)")
+
+
+        
 
         let innerSvg = svg
             .append("g")
@@ -224,8 +239,31 @@ window.addEventListener('load', () => {
             .call(d3.axisLeft(y).tickSize(0))
             .selectAll("g")
             .selectAll("text")
-            .attr("font-size", 10)
+            .attr("color", 'black')
+            .attr("font-size", 7)
             .attr("font-family", "'Ubuntu Mono', Monaco, monospace");
+        
+        // Set-up the export button
+        d3.select('#saveButton').on('click', function(){
+            // addInlineStyling([svg]);
+            // addInlineStyling(svg.selectAll('.bar-chart rect').nodes());
+            d3.selectAll('#svg3, #svg3 .bar-chart rect, .axis-gridline .domain, .tick line').each(function() {
+                var element = this;
+                var computedStyle = getComputedStyle(element, null);
+                for (var i = 0; i < computedStyle.length; i++) {
+                  var property = computedStyle.item(i);
+                  var value = computedStyle.getPropertyValue(property);
+                  element.style[property] = value;
+                }
+              });
+            console.log(svg.selectAll('.bar-chart rect').nodes())
+            var svgString = getSVGString(svg.node());
+            svgString2Image( svgString, 4*boxWidth, 4*boxHeight, 'png', save ); // passes Blob and filesize String to the callback
+
+            function save( dataBlob, filesize ){
+                saveAs( dataBlob, 'D3 vis exported to PNG.png' ); // FileSaver.js function
+            }
+        });
     });
 });
 
@@ -339,3 +377,136 @@ window.addEventListener('load', () => {
         ajax: search
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function addInlineStyling(elements) {
+    if(elements && elements.length) {
+      elements.forEach(function(d) {
+       d3.selectAll(d.el).each(function(){
+        var element = this;
+        if(d.properties && d.properties.length) {
+          d.properties.forEach(function(prop) {
+            var computedStyle = getComputedStyle(element, null),
+              value = computedStyle.getPropertyValue(prop);
+            console.log(computedStyle);
+            element.style[prop] = value;
+          });
+        }
+      });
+     });
+    }
+  }
+
+// Below are the functions that handle actual exporting:
+// getSVGString ( svgNode ) and svgString2Image( svgString, width, height, format, callback )
+function getSVGString( svgNode ) {
+    svgNode.setAttribute('xlink', 'http://www.w3.org/1999/xlink');
+    var cssStyleText = getCSSStyles( svgNode );
+    appendCSS( cssStyleText, svgNode );
+
+    var serializer = new XMLSerializer();
+    var svgString = serializer.serializeToString(svgNode);
+    svgString = svgString.replace(/(\w+)?:?xlink=/g, 'xmlns:xlink='); // Fix root xlink without namespace
+    svgString = svgString.replace(/NS\d+:href/g, 'xlink:href'); // Safari NS namespace fix
+
+    return svgString;
+
+    function getCSSStyles( parentElement ) {
+        var selectorTextArr = [];
+
+        // Add Parent element Id and Classes to the list
+        selectorTextArr.push( '#'+parentElement.id );
+        for (var c = 0; c < parentElement.classList.length; c++)
+                if ( !contains('.'+parentElement.classList[c], selectorTextArr) )
+                    selectorTextArr.push( '.'+parentElement.classList[c] );
+
+        // Add Children element Ids and Classes to the list
+        var nodes = parentElement.getElementsByTagName("*");
+        for (var i = 0; i < nodes.length; i++) {
+            var id = nodes[i].id;
+            if ( !contains('#'+id, selectorTextArr) )
+                selectorTextArr.push( '#'+id );
+
+            var classes = nodes[i].classList;
+            for (var c = 0; c < classes.length; c++)
+                if ( !contains('.'+classes[c], selectorTextArr) )
+                    selectorTextArr.push( '.'+classes[c] );
+        }
+
+        // Extract CSS Rules
+        var extractedCSSText = "";
+        for (var i = 0; i < document.styleSheets.length; i++) {
+            var s = document.styleSheets[i];
+            
+            try {
+                if(!s.cssRules) continue;
+            } catch( e ) {
+                    if(e.name !== 'SecurityError') throw e; // for Firefox
+                    continue;
+                }
+
+            var cssRules = s.cssRules;
+            for (var r = 0; r < cssRules.length; r++) {
+                if ( contains( cssRules[r].selectorText, selectorTextArr ) )
+                    extractedCSSText += cssRules[r].cssText;
+            }
+        }
+        
+
+        return extractedCSSText;
+
+        function contains(str,arr) {
+            return arr.indexOf( str ) === -1 ? false : true;
+        }
+
+    }
+
+    function appendCSS( cssText, element ) {
+        var styleElement = document.createElement("style");
+        styleElement.setAttribute("type","text/css"); 
+        styleElement.innerHTML = cssText;
+        var refNode = element.hasChildNodes() ? element.children[0] : null;
+        element.insertBefore( styleElement, refNode );
+    }
+}
+
+
+function svgString2Image( svgString, width, height, format, callback ) {
+    var format = format ? format : 'png';
+
+    var imgsrc = 'data:image/svg+xml;base64,'+ btoa( unescape( encodeURIComponent( svgString ) ) ); // Convert SVG string to data URL
+
+    var canvas = document.createElement("canvas");
+    var context = canvas.getContext("2d");
+
+    canvas.width = width;
+    canvas.height = height;
+
+    var image = new Image();
+    image.onload = function() {
+        context.clearRect ( 0, 0, width, height );
+        context.drawImage(image, 0, 0, width, height);
+
+        canvas.toBlob( function(blob) {
+            var filesize = Math.round( blob.length/1024 ) + ' KB';
+            if ( callback ) callback( blob, filesize );
+        });
+
+        
+    };
+
+    image.src = imgsrc;
+}
