494038e1f454d1f5f26ca2d338902183eff7bfd9
seedubjay <cwj@dhcp-10-249-65-243.eduroam.wireless.private.cam.ac.uk>
2021-02-23 18:36:48 +0000

    lazy hello

--- assets/js/interactive-problems.js
+++ assets/js/interactive-problems.js
@@ -6,44 +6,58 @@ let N = 100;
 
 let uuid = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 
-let session_id = null;
+let hotter_session_id = null;
+function get_hotter_session_id() {
+    if (hotter_session_id) {
+        return Promise.resolve(hotter_session_id);
+    } else {
+        return fetch(`https://api.seedubjay.com/problems/hotter/hello/${uuid}?custom_n=100`)
+            .then(resp => {
+                return resp.text().then(text => {
+                    if (resp.ok) {
+                        hotter_session_id = text;
+                        return Promise.resolve(text);
+                    } else return Promise.reject(text);
+                })
+            })
+    }
+}
 
 function hotter_reset() {
-    fetch(`https://api.seedubjay.com/problems/hotter/hello/${uuid}?custom_n=100`)
-        .then(resp => {
-            if (resp.ok) {
-                resp.text().then(text => {
-                    session_id = text;
-                })
-                hotter_output.innerHTML = '';
-                hotter_error.innerHTML = '';
-            }
-        })
+    hotter_session_id = null;
+    hotter_output.innerHTML = '';
+    hotter_error.innerHTML = '';
 }
-hotter_reset()
 
 function hotter_guess() {
     let v = hotter_input.value;
     if (!v || v < 1 || v > N) return;
-    fetch(`https://api.seedubjay.com/problems/hotter/guess/${session_id}/${v}`)
-        .then(resp => {
-            if (resp.ok) {
-                resp.text().then(text => {
-                    hotter_output.innerHTML += `<div>${v} - ${text}</div>`
-                    hotter_error.innerHTML = '';
+    get_hotter_session_id()
+        .then(session_id => {
+            console.log(session_id);
+            fetch(`https://api.seedubjay.com/problems/hotter/guess/${session_id}/${v}`)
+                .then(resp => {
+                    if (resp.ok) {
+                        resp.text().then(text => {
+                            hotter_output.innerHTML += `<div>${v} - ${text}</div>`
+                            hotter_error.innerHTML = '';
+                        })
+                    } else {
+                        resp.text().then(error => {
+                            hotter_error.innerHTML = error;
+                        })
+                    }
                 })
-            } else {
-                resp.text().then(error => {
-                    hotter_error.innerHTML = error;
-                })
-            }
+        })
+        .catch(err => {
+            console.log(err);
         })
 }
 
 hotter_input.addEventListener('keyup', event => {
     if (event.keyCode == 13) {
         hotter_guess();
-        hotter_input.value = ''
+        // hotter_input.value = ''
     }
 })
 
